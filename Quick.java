6bbb6fe6803067f104c282caf8d8575960aeda98
athompson00 <athompson00@stuy.edu>
2019-03-13 11:02:44 -0400

    Quick.java

--- Quick.java
+++ Quick.java
@@ -7,9 +7,10 @@ public class Quick{
     int a = data[n];
     data[n] = data[start];
     data[start] = a;
-    for (int i = start; i <= end; i++){
+    for (int i = start; i < end + 1; i++){
+      //a = data[i];
       if (a > data[i]){
-        int z = data[start];
+        int z = data[i];
         data[start] = data[i];
         for (int j = i - 1; j > start; j--){
           if (j == start + 1){
@@ -28,14 +29,35 @@ public class Quick{
     return -1;
   }
 
+
+  public static int quickselect(int []data, int k){
+    return 1;
+  }
+
   public static void main(String[] args){
-    int[] a = {1, 2, 3, 4, 5, 6};
-    partition(a, 0, 5);
+    int[] a = new int[100];
+    for (int i = 100; i > 0; i--){
+      a[100 - i] = i;
+    }
+    partition(a, 0, 99);
     String n = "[";
-    for (int i = 0; i < 6; i++){
-      n += a[i];
+    for (int i = 0; i < a.length; i++){
+      n += " " + a[i];
     }
     n += "]";
     System.out.println(n);
+  }
+
+  public static void quickSort(int[] ary){
+    quickSortHelp(ary, 0, ary.length - 1);
+  }
+
+  public static void quickSortHelp(int[] ary, int lo, int hi){
+    if (lo >= hi){
+      return;
+    }
+    int pivot = partition(ary, lo, hi);
+    quickSortHelp(ary, pivot + 1, hi);
+    quickSortHelp(ary, lo, pivot - 1);
   }
 }
